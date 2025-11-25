12eb4011d727927a321ab0e32835e81aea313e4a
allindiacoderlife <chiragsaxena728@gmail.com>
2025-11-25 11:13:43 +0530

    Add Text component for displaying text files; update App and index.js to include Text

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import gsap from "gsap";
 import { Draggable } from "gsap/all";
 
 import { Dock, Navbar, Welcome } from "#components";
-import { Safari, Terminal, Resume, Finder } from "#windows";
+import { Safari, Terminal, Resume, Finder, Text } from "#windows";
 
 gsap.registerPlugin(Draggable);
 
@@ -17,6 +17,7 @@ const App = () => {
       <Safari />
       <Resume />
       <Finder />
+      <Text />
     </main>
   );
 };
