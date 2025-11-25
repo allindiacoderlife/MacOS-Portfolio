fdd394ff77a587ec421ddec8bababa7737a8be1d
allindiacoderlife <chiragsaxena728@gmail.com>
2025-11-25 11:39:48 +0530

    feat: add Contact component with social links and email information

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import gsap from "gsap";
 import { Draggable } from "gsap/all";
 
 import { Dock, Navbar, Welcome } from "#components";
-import { Safari, Terminal, Resume, Finder, Text, Image } from "#windows";
+import { Safari, Terminal, Resume, Finder, Text, Image, Contact } from "#windows";
 
 gsap.registerPlugin(Draggable);
 
@@ -19,6 +19,7 @@ const App = () => {
       <Finder />
       <Text />
       <Image />
+      <Contact />
     </main>
   );
 };
